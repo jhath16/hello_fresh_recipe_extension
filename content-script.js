5fb397c02b55c0552256c20c8903976a3a0aac2c
Justin Hathaway <hathaway.justin@gmail.com>
2021-10-10 01:47:54 -0400

    initial commit

---
+++ content-script.js
@@ -0,0 +1,14 @@
+// Add an element to the page that allows you to add recipes to list
+
+// Onclick of "add recipe", grab the ingredient info
+// Parse the ingredient info to the actual values
+
+
+// Add the recipe and ingredient list to our grocery list
+// (Probably going to need to use the storage API)
+
+// Questions
+// Which page will the final list go to? 
+
+// The extension popup.html should show the currently added 
+// recipes and give options to remove them.
