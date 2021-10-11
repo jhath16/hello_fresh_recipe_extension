5fef8f0a241fd0cdf5e0fa1f82c8b495d5f20d3d
jhath16 <hathaway.justin@gmail.com>
2021-10-11 02:44:53 -0400

    refactor some html parsing and run some functions on document ready

--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 chrome.storage.local.onChanged.addListener(function (changes, namespace) {
   console.log(changes);
+  chrome.storage.local.get(null, function (recipes) {
+    for (let i = 0; i < recipes.length; i++) {
+      console.log(recipe[i]);
+    }
+  });
 });
 
-document.querySelector("a").addEventListener("click",function () {
-  alert('click')
-  return false;
-})
--- src/content-script.js
+++ src/content-script.js
@@ -9,40 +9,60 @@ function getFromStorage (key) {
     });
 }
 
-// Add an element to the page that allows you to add recipes to list
+// query dom by text content / tag name
+function querySelectorIncludesText (selector, text){
+    return Array.from(document.querySelectorAll(selector))
+      .find(el => el.textContent.includes(text));
+  }
 
-let newEl = document.querySelector("[data-test-id='deliveryButton'").cloneNode(true);
-newEl.children[0].textContent = "Add to list";
-newEl.style.marginRight = "14px";
-document.querySelector(".dsw.dsjf.dsjg").prepend(newEl);
+document.onreadystatechange = function (e) {
+    if (document.readyState != "complete") return;
+    // Add an element to the page that allows you to add recipes to list
+    const deliveryButton = document.querySelector("[data-test-id='deliveryButton'");
+    let newEl = deliveryButton.cloneNode(true);
+    newEl.children[0].textContent = "Add to list";
+    newEl.style.marginRight = "14px";
+    deliveryButton.parentNode.prepend(newEl);
 
-// Initialize recipe object
-let recipe = {
-    ingredients: [],
-};
-
-// Onclick of "add recipe", grab the ingredient info
-newEl.addEventListener('click', (e) => {
-    // Parse the ingredient info to the actual values
-    let ingredientEls = document.querySelectorAll(".fela-_g6xips .fela-_1qz307e");
-    for (let i = 0; i < ingredientEls.length; i++) {
-        let el = ingredientEls[i];
-        let amount = el.children[0].textContent;
-        let ingredient = el.children[1].textContent;
-
-        console.log(`we need ${amount} of ${ingredient}`);
-        recipe.ingredients.push({
-            amount: amount,
-            ingredient: ingredient
-        });
-    }
-    let key = "recipe" + Date.now(); // change date to recipe route in URL
-    chrome.storage.local.set({[key]: recipe});
-    // Add the recipe and ingredient list to our grocery list
-    // (Probably going to need to use the storage API)
-});
-
-
+    // Onclick of "add recipe", grab the ingredient info
+    newEl.addEventListener('click', (e) => {
+        // Get metadata about recipe
+        let recipeName = document.querySelector("[data-test-id='recipeDetailFragment.recipe-name']").textContent;
+        let recipeSides = document.querySelector("[data-test-id='recipeDetailFragment.recipe-name']").nextSibling.textContent;
+        let recipeTime = document.querySelector("[data-translation-id='recipe-detail.preparation-time']").parentNode.nextSibling.textContent;
+        let recipeDescription = document.querySelector("p").textContent; // this should always be the first p tag on page but may need to change
+    
+        // Initialize recipe object
+        let recipe = {
+            name: recipeName,
+            sides: recipeSides,
+            time: recipeTime,
+            description: recipeDescription,
+            ingredients: [],
+        };
+    
+        // Parse the ingredient info to the actual values
+        let ingredientEls = document.querySelectorAll(".fela-_g6xips .fela-_1qz307e");
+        for (let i = 0; i < ingredientEls.length; i++) {
+            let el = ingredientEls[i];
+            let amount = el.children[0].textContent;
+            let ingredient = el.children[1].textContent;
+    
+            recipe.ingredients.push({
+                amount: amount,
+                ingredient: ingredient
+            });
+        }
+        // Create unique key for all recipes so we do not overwrite them...
+        // Will we need to change the way these are stored if we are storign them by week?
+    
+        let key = "recipe_" + window.location.pathname.replace('/recipes/', '');
+        console.log(recipe);
+        chrome.storage.local.set({[key]: recipe});
+        // Add the recipe and ingredient list to our grocery list
+        // (Probably going to need to use the storage API)
+    });
+}
 
 // Questions
 // Which page will the final list go to? 
